d46f3dab69287b6920a0cc7e52a546a847226f80
cppakko <a1378774701@gmail.com>
2020-11-28 21:47:18 +0800

    优化冗余代码

--- src/main/java/akko/ddbot/listener/ImgSeachListener.kt
+++ src/main/java/akko/ddbot/listener/ImgSeachListener.kt
@@ -4,9 +4,7 @@ import akko.ddbot.InitCheck
 import akko.ddbot.data.CQImageData
 import akko.ddbot.data.GetMsgData
 import akko.ddbot.network.SaucenaoApiService
-import akko.ddbot.utilities.GlobalObject
-import akko.ddbot.utilities.groupMsg
-import akko.ddbot.utilities.SauceHelper
+import akko.ddbot.utilities.*
 import cc.moecraft.icq.event.EventHandler
 import cc.moecraft.icq.event.IcqListener
 import cc.moecraft.icq.event.events.message.EventGroupMessage
@@ -35,50 +33,35 @@ class ImgSeachListener : IcqListener() {
                     if ("id=" == matcher.group(1)) {
                         val isMinus = "-" == matcher.group(2)
                         val messageId = if (isMinus) "-" + matcher.group(3) else matcher.group(3)
-                        val url = "http://0.0.0.0:" + InitCheck.POST_PORT + "/get_msg?message_id=" + messageId + "&access_token=" + InitCheck.ACCESS_TOKEN
-                        val client = OkHttpClient()
-                        val request = Request.Builder().url(url).build()
-                        val call = client.newCall(request)
                         try {
-                            val body = call.execute().body()!!.string()
-                            val rawImginfo = GlobalObject.jacksonObjectMapper.readValue<GetMsgData>(body).data.message
+                            val rawImginfo = getMsg(messageId).message
                             pattern = Pattern.compile("(file=)([0-9a-z.]*)")
                             val imgM = pattern.matcher(rawImginfo)
                             if (imgM.find()) {
-                                val getImgRequest = Request.Builder().url("http://0.0.0.0:5700/get_image?file=" + imgM.group(2) + "&access_token=" + InitCheck.ACCESS_TOKEN).build()
-                                client.newCall(getImgRequest).enqueue(object: Callback{
-                                    override fun onFailure(call: Call, e: IOException) {
-                                        groupMsg(InitCheck.GROUP_ID.toLong(),MessageBuilder().add(ComponentImage("amamiya_err.jpg")).toString())
+                                val imgQqUrl = getImg(imgM.group(2)).data.url
+                                val retrofit = Retrofit.Builder().baseUrl("https://saucenao.com/").build()
+                                retrofit.create(SaucenaoApiService::class.java)[999, 2, 1, 1, imgQqUrl, InitCheck.SAUCENAO_API_KEY]?.enqueue(object: retrofit2.Callback<ResponseBody?> {
+                                    override fun onResponse(call: retrofit2.Call<ResponseBody?>, response: retrofit2.Response<ResponseBody?>) {
+                                        val searchBody = response.body()!!.string()
+                                        val result = SauceHelper.extract(searchBody,1)[0]
+                                        val builder = MessageBuilder()
+                                        builder.run {
+                                            add(ComponentImage(result.thumbnail)).newLine()
+                                            for (str: String in result.extUrls) {
+                                                add(str).newLine()
+                                            }
+                                            add("相似度 -> " + result.similarity)
+                                        }
+                                        if (result.similarity < 50) {
+                                            builder.run {
+                                                newLine()
+                                                add("你这图怎么回事啊")
+                                            }
+                                        }
+                                        groupMsg(event.groupId,builder.toString())
                                     }
-                                    override fun onResponse(call: Call, response: Response) {
-                                        val body = response.body()!!.string()
-                                        val imgQqUrl = GlobalObject.jacksonObjectMapper.readValue<CQImageData>(body).data.url
-                                        val retrofit = Retrofit.Builder().baseUrl("https://saucenao.com/").build()
-                                        retrofit.create(SaucenaoApiService::class.java)[999, 2, 1, 1, imgQqUrl, InitCheck.SAUCENAO_API_KEY]?.enqueue(object: retrofit2.Callback<ResponseBody?> {
-                                            override fun onResponse(call: retrofit2.Call<ResponseBody?>, response: retrofit2.Response<ResponseBody?>) {
-                                                val searchBody = response.body()!!.string()
-                                                val result = SauceHelper.extract(searchBody,1)[0]
-                                                val builder = MessageBuilder()
-                                                builder.run {
-                                                    add(ComponentImage(result.thumbnail)).newLine()
-                                                    for (str: String in result.extUrls) {
-                                                        add(str).newLine()
-                                                    }
-                                                    add("相似度 -> " + result.similarity)
-                                                }
-                                                if (result.similarity < 50) {
-                                                    builder.run {
-                                                        newLine()
-                                                        add("你这图怎么回事啊")
-                                                    }
-                                                }
-                                                groupMsg(event.groupId,builder.toString())
-                                            }
-
-                                            override fun onFailure(call: retrofit2.Call<ResponseBody?>, t: Throwable) {
-                                                groupMsg(InitCheck.GROUP_ID.toLong(),MessageBuilder().add(ComponentImage("amamiya_err.jpg")).toString())
-                                            }
-                                        })
+                                    override fun onFailure(call: retrofit2.Call<ResponseBody?>, t: Throwable) {
+                                        groupMsg(InitCheck.GROUP_ID.toLong(),MessageBuilder().add(ComponentImage("amamiya_err.jpg")).toString())
                                     }
                                 })
                             }
