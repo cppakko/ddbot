2f605df35246c887475d62b8ac77464cdc567f86
cppakko <a1378774701@gmail.com>
2020-11-28 21:45:19 +0800

    Update HelpCommand.kt

--- src/main/java/akko/ddbot/command/HelpCommand.kt
+++ src/main/java/akko/ddbot/command/HelpCommand.kt
@@ -24,7 +24,8 @@ class HelpCommand : GroupCommand {
             add(" !help 来查看你现在正在看的帮助").newLine()
             add(" !setu 懂的都懂").newLine()
             add(" 对图片回复 给我翻译翻译 就会给你翻译啦(确信 ").newLine()
-            add(" 对图片回复 NAONAO 就会给你NAO一下(大嘘 ")
+            add(" 对图片回复 NAONAO 就会给你NAO一下(大嘘 ").newLine()
+            add(" 对图片回复 这个好 就...").newLine()
             add(" 输入!dice help获取dice帮助").newLine()
             add("----------").toString()
         }
