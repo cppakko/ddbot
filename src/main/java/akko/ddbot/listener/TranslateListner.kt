7483c31bd39ec749f8f3f043eaf6bb55fa06ebe4
cppakko <a1378774701@gmail.com>
2020-11-29 10:41:56 +0800

    Update TranslateListner.kt

--- src/main/java/akko/ddbot/listener/TranslateListner.kt
+++ src/main/java/akko/ddbot/listener/TranslateListner.kt
@@ -23,25 +23,28 @@ class TranslateListener : IcqListener() {
             val regex = "(id=)(-*)(\\d*)"
             pattern = Pattern.compile(regex)
             val matcher = pattern.matcher(rawMessage)
-            if (matcher.find()) {
+            if (matcher.find())
+            {
                 if ("id=" == matcher.group(1)) {
                     val isMinus = "-" == matcher.group(2)
                     val messageId = if (isMinus) "-" + matcher.group(3) else matcher.group(3)
                     try {
                         pattern = Pattern.compile("(file=)([0-9a-z.]*)")
-                        val imgM = pattern.matcher(getMsg(messageId).message)
-                        if (imgM.find()) {
+                        val rowMessage = getMsg(messageId).message
+                        val imgM = pattern.matcher(rowMessage)
+                        val transApi = TransApi(InitCheck.BAIDU_APP_ID!!, InitCheck.BAIDU_SECURITY_KEY!!)
+                        val mb = MessageBuilder()
+                        if (imgM.find())
+                        {
                             val data = ocrFun(imgM.group(2).toString()).data!!
                             val language = data.language
                             val textsList = data.texts!!
-                            val mb = MessageBuilder()
                             if ("zh" == language) {
                                 for (t in textsList) {
                                     mb.add(t.text)
                                     mb.newLine()
                                 }
                             } else {
-                                val transApi = TransApi(InitCheck.BAIDU_APP_ID!!, InitCheck.BAIDU_SECURITY_KEY!!)
                                 for (t in textsList) {
                                     val transJS = transApi.getTransResult(t.text!!, "auto", "zh")
                                     val unicode = GlobalObject.objectMapper.readValue(transJS, TranslateData::class.java).transResult!![0].dst
@@ -49,8 +52,15 @@ class TranslateListener : IcqListener() {
                                     mb.newLine()
                                 }
                             }
-                            event.respond(mb.toString())
                         }
+                        else
+                        {
+                            val transJS = transApi.getTransResult(rowMessage, "auto", "zh")
+                            val unicode = GlobalObject.objectMapper.readValue(transJS, TranslateData::class.java).transResult!![0].dst
+                            mb.add(unicode)
+                            mb.newLine()
+                        }
+                        event.respond(mb.toString())
                     } catch (e: IOException) {
                         event.respond(e.message)
                         e.printStackTrace()
